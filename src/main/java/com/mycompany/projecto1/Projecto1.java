f15935605bdb3622a2aa1438630872fdc9ef281c
Pere Antoni <Pere Antoni@DESKTOP-HJDQ6UB>
2023-10-02 22:54:46 +0200

    Presentacion

---
+++ src/main/java/com/mycompany/projecto1/Projecto1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projecto1;
+
+/**
+ *
+ * @author Pere Antoni
+ */
+public class Projecto1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Pere Antoni");
+    }
+}
